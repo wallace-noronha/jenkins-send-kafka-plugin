24f641e91d796c1adf923a8fae3280c9bf7266cd
t734728 <wallace.noronha@santander.com.br>
2020-12-29 13:40:54 -0300

    Alter implementation

--- src/main/java/io/jenkins/plugins/KafkaProducerMessage.java
+++ src/main/java/io/jenkins/plugins/KafkaProducerMessage.java
@@ -10,6 +10,7 @@ public class KafkaProducerMessage {
 
     private String topic;
     private String broker;
+    ProducerProperties producerProperties = new ProducerProperties();
 
     public KafkaProducerMessage(String broker, String topic){
         this.broker = broker;
@@ -18,24 +19,14 @@ public class KafkaProducerMessage {
 
     public void produce(String message){
 
-        // create instance for properties to access producer configs
-        Properties props = new Properties();
+        try {
 
-        //Assign localhost id
-        props.put("bootstrap.servers", broker);
-        //Set acknowledgements for producer requests.
-        props.put("acks", "all");
-        //Specify buffer size in config
-        props.put("batch.size", 16384);
-        //Reduce the no of requests less than 0
-        props.put("linger.ms", 1);
-        //The buffer.memory controls the total amount of memory available to the producer for buffering.
-        props.put("buffer.memory", 33554432);
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-
-        Producer<String, String> producer = new KafkaProducer<String, String>(props);
-        producer.send(new ProducerRecord<String, String>(topic, message));
-        producer.close();
+            Producer<String, String> producer = new KafkaProducer<String, String>(producerProperties.getProperties(broker));
+            System.out.println("TRY TO SEND MESSAGE....");
+            producer.send(new ProducerRecord<String, String>(topic, message));
+            System.out.println("SUCCESS SEND MESSAGE!!!");
+        } catch (Exception e){
+            System.out.println("Erro to send message, try again in a feel later!");
+        }
     }
 }
---
+++ src/main/java/io/jenkins/plugins/ProducerProperties.java
@@ -0,0 +1,31 @@
+package io.jenkins.plugins;
+
+import java.util.Properties;
+
+public class ProducerProperties {
+
+    public Properties getProperties(String broker){
+        // create instance for properties to access producer configs
+        Properties props = new Properties();
+
+        //Assign localhost id
+        props.put("bootstrap.servers", broker);
+        //Set acknowledgements for producer requests.
+        props.put("acks", "all");
+        //Set timeout from send
+        props.put("timeout.ms",3000);
+        //If the request fails, the producer can automatically retry,
+        props.put("retries", 5);
+        //Specify buffer size in config
+        props.put("batch.size", 16384);
+        //Reduce the no of requests less than 0
+        props.put("linger.ms", 1);
+        //The buffer.memory controls the total amount of memory available to the producer for buffering.
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        return props;
+    }
+
+}
